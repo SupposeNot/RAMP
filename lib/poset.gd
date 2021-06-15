3f7c3088d3f60d4d40d88be645b897d13a5cbf26
Gordon Williams <supposenot@me.com>
2021-06-14 21:38:00 -0800

    Setting up and using the category Poset
    Still a way to go to handle all the cases, but NON full posets work everywhere.

--- lib/poset.gd
+++ lib/poset.gd
@@ -1,16 +1,27 @@
 # Dealing with maniplexes as posets
 
-DeclareCategory("IsPoset", IsObject);
 
-#in each of the represenations below, at some level, the object is going to be a list. At this point, the expectation is that all of them will have some way of storing the flags in a face, and some way of accessing the rank of the face (either by position or by label).
 
-#DeclareRepresentation("IsPosetAsListOfListsByRank",IsComponentObjectRep and IsAttributeStoringRep,["ListedByIncreasingRank"]);
-#DeclareRepresentation("IsPosetAsListOfListsByRank",IsComponentObjectRep and IsAttributeStoringRep,["ListedByIncreasingRankWithRanks"]);
-#DeclareRepresentation("IsPosetAsListOfListsByRank",IsComponentObjectRep and IsAttributeStoringRep,["FacesWithRanks"]);
 
 
 #! @Chapter Combinatorics and Structure
 #! @Section Posets
+
+#! @Arguments list
+#! Given a list of lists of faces in increasing rank, where each face is described by the incident flags, gives you a IsPosetOfFlags object back.
+DeclareOperation("PosetFromFaceListOfFlags",[IsList]);
+
+DeclareAttribute("IsFullPoset",IsPoset,"mutable");
+DeclareAttribute("RankPoset",IsPoset);
+DeclareAttribute("Flaggable",IsPoset);
+
+#DeclareAttribute("RankManiplex", IsManiplex);
+
+#! @Arguments poset
+#! Given a <A>poset</A>, returns the rank of the poset. Note: There may be hidden assumptions here to untangle later.
+DeclareOperation("RankOfPoset", [IsPosetOfFlags]);
+
+
 
 #! @Arguments g
 #! Given a group, returns a poset as a list of faces ordered by rank, where each face is represented as a list of the flags it contains. Note that this function does not include the minimal (empty) face nor the maximal face of the maniplex. Note that the <A>i</A>-faces correspond to the <A>i+1</A> item in the list because of how GAP indexes lists.
@@ -33,24 +44,30 @@ DeclareOperation("AreIncidentFaces",[IsObject,IsObject]);
 
 #! @Arguments poset
 #! Given a poset, (not a fullposet) this will give you a version of the list of flags in terms of the faces described in the poset.
-DeclareOperation("FlagsAsListOfFacesFromPoset",[IsList]); 
+DeclareOperation("FlagsAsListOfFacesFromPoset",[IsPosetOfFlags]); 
 
 
-#! @Arguments flag,flags,i
-#! Given a flag and a list of flags (represented as chains of faces comprised of lists of flags) and a rank, this function will give you the <A>i</A>-adjacent flag. Note that adjacencies are listed from ranks 0 to one less than the dimension.
-DeclareOperation("AdjacentFlag",[IsList,IsList,IsInt]);
+#! @Arguments flag,poset,i
+#! Given a flag (represented as chains of faces comprised of lists of flags) and a poset  and a rank, this function will give you the <A>i</A>-adjacent flag. Note that adjacencies are listed from ranks 0 to one less than the dimension.
+#! You can replace <A>flag</A> with the integer corresponding to that flag.
+#DeclareOperation("AdjacentFlag",[IsList,IsList,IsInt]);
+DeclareOperation("AdjacentFlag",[IsList,IsPosetOfFlags,IsInt]);
 
 #! @Arguments poset, i
 #! Given a <A>poset</A> and an integer <A>i</A>, this function will give you the associated permutation for the rank <A>i</A>-connection.
-DeclareOperation("ConnectionGeneratorOfPoset",[IsList,IsInt]);
+#DeclareOperation("ConnectionGeneratorOfPoset",[IsList,IsInt]);
+DeclareOperation("ConnectionGeneratorOfPoset",[IsPosetOfFlags,IsInt]);
 
 
 #! @Arguments poset
 #! Given a <A>poset</A> corresponding to a maniplex, this function will give you the connection group. Note that it assumes we are NOT using a full poset (currently).
-DeclareOperation("ConnectionGroupOfPoset",[IsList]);
-
+DeclareOperation("ConnectionGroupOfPoset",[IsPosetOfFlags]);
 
 
 #! @Arguments poset
 #! Given a <A>poset</A> (whose elements are lists of flags) corresponding to a maniplex, this function will tell you if it is flaggable, i.e., if the flags can be recovered from the poset or not.
-DeclareOperation("IsFlaggablePoset",[IsList]);
+DeclareOperation("IsFlaggablePoset",[IsPosetOfFlags]);
+
+#! @Arguments list
+#! Given <A>list</A>, a poset as a list of faces ordered by rank, each face listing the flags on the face, this function will tell you if the poset is full or not.
+DeclareOperation("ListIsFullPoset",[IsList]);
--- lib/types.gd
+++ lib/types.gd
@@ -1,9 +1,13 @@
 DeclareCategory("IsManiplex", IsObject);
 DeclareCategory("IsRotaryManiplex", IsManiplex);
 DeclareCategory("IsReflexibleManiplex", IsRotaryManiplex);
+DeclareCategory("IsPoset", IsObject);
 
 DeclareRepresentation("IsReflexibleManiplexWithRels", IsComponentObjectRep and IsAttributeStoringRep, ["aut_gp", "rels", "schlafli_symbol", "fvec"]);
 DeclareRepresentation("IsReflexibleManiplexWithoutRels", IsComponentObjectRep and IsAttributeStoringRep, ["aut_gp", "schlafli_symbol", "fvec"]);
 DeclareRepresentation("IsRotaryManiplexRep", IsComponentObjectRep and IsAttributeStoringRep, ["rot_gp", "rels", "size", "fvec"]);
 DeclareRepresentation("IsManiplexConnGpRep", IsComponentObjectRep and IsAttributeStoringRep, ["conn_gp", "fvec"]);
 
+#Poset stuff
+DeclareRepresentation("IsPosetOfFlags", IsComponentObjectRep and IsAttributeStoringRep,["faces_list_by_rank"]);
+DeclareRepresentation("IsPosetByElementsWParents", IsComponentObjectRep and IsAttributeStoringRep,["face_parent_pairs"]);
