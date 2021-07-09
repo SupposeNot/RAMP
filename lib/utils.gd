7920794cea8d0dcade0f03bbd92f167046e166c1
Gabe Cunningham <gabriel.cunningham@gmail.com>
2021-07-08 20:48:18 -0400

    Document some utility functions.

--- lib/utils.gd
+++ lib/utils.gd
@@ -1,7 +1,19 @@
+
+#! @Arguments args
+#! @Description Calls `Maniplex(args)` and marks the output as polytopal.
 DeclareGlobalFunction("AbstractPolytope");
+
+#! @Arguments args
+#! @Description Calls `ReflexibleManiplex(args)` and marks the output as polytopal.
+#! Also available as `ARP(args)`.
 DeclareGlobalFunction("AbstractRegularPolytope");
-DeclareGlobalFunction("AbstractRotaryPolytope");
+
 DeclareSynonym("ARP", AbstractRegularPolytope);
+
+#! @Arguments args
+#! @Description Calls `RotaryManiplex(args)` and marks the output as polytopal.
+DeclareGlobalFunction("AbstractRotaryPolytope");
+
 
 DeclareGlobalFunction("TranslatePerm");
 DeclareGlobalFunction("MultPerm");
@@ -13,4 +25,9 @@ DeclareGlobalFunction("StandardizeSggi");
 
 DeclareGlobalFunction("IsSameRank");
 
+#! @Arguments L, x
+#! Given a list <A>L</A> and an object <A>x</A>, this calls
+#! Append(L, x) if x is a list; otherwise it calls Add(L, x).
+#! Note that since strings are internally represented as lists,
+#! AddOrAppend(L, "foo") will append the characters 'f', 'o', 'o'.
 DeclareGlobalFunction("AddOrAppend");
