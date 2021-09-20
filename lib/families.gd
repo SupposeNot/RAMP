d3a542dc83a594c1df1a27b36246b6d5969eec79
Gordon Williams <supposenot@me.com>
2021-09-20 09:32:10 -0800

    Autodoc corrections

--- lib/families.gd
+++ lib/families.gd
@@ -3,78 +3,81 @@
 #! @Section Classical Polytopes
 
 #! @Arguments
-#! Returns the universal 0-polytope.
+#! @Returns the universal 0-polytope.
 DeclareOperation("Vertex", []);
 
 #! @Arguments
-#! Returns the universal 1-polytope.
+#! @Returns the universal 1-polytope.
 DeclareOperation("Edge", []);
 
 #! @Arguments p
-#! Returns the p-gon.
+#! @Returns the p-gon.
 DeclareOperation("Pgon", [IsInt]);
 
 #! @Arguments n
-#! Returns the n-cube.
+#! @Returns the n-cube.
 DeclareOperation("Cube", [IsInt]);
 
 #! @Arguments n
-#! Returns the n-hemi-cube.
+#! @Returns the n-hemi-cube.
 DeclareOperation("HemiCube", [IsInt]);
 
 #! @Arguments n
-#! Returns the n-cross-polytope.
+#! @Returns the n-cross-polytope.
 DeclareOperation("CrossPolytope", [IsInt]);
 
 #! @Arguments n
-#! Returns the n-hemi-cross-polytope.
+#! @Returns the n-hemi-cross-polytope.
 DeclareOperation("HemiCrossPolytope", [IsInt]);
 
 #! @Arguments n
-#! Returns the n-simplex.
+#! @Returns the n-simplex.
 DeclareOperation("Simplex", [IsInt]);
 
 #! @Arguments n
-#! Returns the rank n+1 polytope; the tiling of E^n by n-cubes.
+#! @Returns the rank n+1 polytope; the tiling of E^n by n-cubes.
 DeclareOperation("CubicTiling", [IsInt]);
 
 #! @Arguments
-#! Returns the dodecahedron, {5, 3}.
+#! @Returns the dodecahedron, {5, 3}.
 DeclareOperation("Dodecahedron", []);
 
 #! @Arguments
-#! Returns the hemi-dodecahedron, {5, 3}_5.
+#! @Returns the hemi-dodecahedron, {5, 3}_5.
 DeclareOperation("HemiDodecahedron", []);
 
 #! @Arguments
-#! Returns the icosahedron, {3, 5}.
+#! @Returns the icosahedron, {3, 5}.
 DeclareOperation("Icosahedron", []);
 
 #! @Arguments
-#! Returns the hemi-icosahedron, {3, 5}_5.
+#! @Returns the hemi-icosahedron, {3, 5}_5.
 DeclareOperation("HemiIcosahedron", []);
 
 #! @Arguments
-#! Returns the 24-cell, {3, 4, 3}.
+#! @Returns the 24-cell, {3, 4, 3}.
 DeclareOperation("24Cell", []);
 
 #! @Arguments
-#! Returns the hemi-24-cell, {3, 4, 3}_6.
+#! @Returns the hemi-24-cell, {3, 4, 3}_6.
 DeclareOperation("Hemi24Cell", []);
 
 #! @Arguments
-#! Returns the 120-cell, {5, 3, 3}.
+#! @Returns the 120-cell, {5, 3, 3}.
 DeclareOperation("120Cell", []);
 
 #! @Arguments
-#! Returns the hemi-120-cell, {5, 3, 3}_15.
+#! @Returns the hemi-120-cell, {5, 3, 3}_15.
 DeclareOperation("Hemi120Cell", []);
 
 #! @Arguments
-#! Returns the 600-cell, {3, 3, 5}.
+#! @Returns the 600-cell, {3, 3, 5}.
 DeclareOperation("600Cell", []);
 
 #! @Arguments
-#! Returns the hemi-600-cell, {3, 3, 5}_15.
+#! @Returns the hemi-600-cell, {3, 3, 5}_15.
 DeclareOperation("Hemi600Cell", []);
 
+#! @Arguments
+#! @Returns Bruckner's sphere.
+DeclareOperation("BrucknerSphere",[]);
--- lib/families.gi
+++ lib/families.gi
@@ -233,3 +233,9 @@ InstallMethod(Hemi600Cell,
 	function()
 	return AbstractRegularPolytope("{3,3,5}_15" : set_schlafli);
 	end);
+
+InstallMethod(BrucknerSphere,
+	[],
+	function()
+	return PosetFromAtomicList([ [ 1, 2, 3, 4 ], [ 1, 2, 3, 7 ], [ 1, 2, 6, 7 ], [ 1, 3, 4, 7 ], [ 1, 5, 6, 7 ], [ 2, 3, 4, 5 ],  [ 2, 3, 6, 7 ], [ 3, 4, 6, 7 ], [ 3, 4, 5, 6 ], [ 4, 5, 6, 7 ], [ 2, 3, 5, 8 ], [ 2, 3, 6, 8 ],  [ 3, 5, 6, 8 ], [ 1, 2, 6, 8 ], [ 1, 5, 6, 8 ], [ 1, 2, 4, 8 ], [ 2, 4, 5, 8 ], [ 1, 4, 7, 8 ], [ 1, 5, 7, 8 ], [ 4, 5, 7, 8 ] ]);
+	end);
