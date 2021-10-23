6a2774408dff3d1fa0ca65e042e67c8af6c19b60
Gordon Williams <supposenot@me.com>
2021-10-23 09:41:46 -0800

    Fixes for documentation
    Problem appears to have been in rotary.gd.

--- lib/stratified.gd
+++ lib/stratified.gd
@@ -2,7 +2,11 @@
 #! @Chapter Stratified Operations
 #! @Section Computational tools
 
+#! Some description.
+
+
 #! @Arguments element1, element2
-#! @Returns product element
-#! @Description Elements are ordered pairs of the form [perm, list], where the elements of list are members of a group. Operation performed is consistent with that in defined in <Cite Key="PelWil18">.
+#! @Returns element
+#! @Description Elements are ordered pairs of the form [perm, list], where the elements of list are members of a group. Operation performed is consistent with that in defined in PelWil18.
+
 DeclareOperation("StratifiedProduct", [IsList,IsList]);
