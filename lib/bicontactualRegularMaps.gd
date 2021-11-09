4743ed1dbc3bfe897531e483dc72bc849f9d4cd2
Gordon Williams <supposenot@me.com>
2021-11-09 09:42:37 -0900

    Bicontactual
    Added tests and example sessions

--- lib/bicontactualRegularMaps.gd
+++ lib/bicontactualRegularMaps.gd
@@ -7,32 +7,63 @@
 #! @Returns maniplex
 #! @Description Given an integer <A>k</A>, gives the map $\epsilon_k$, which is $\{k,2\}_k$ when <A>k</A> is even, and $\{k,2\}_{2k}$ when <A>k</A> is odd.
 DeclareOperation("Epsilonk",[IsInt]);
+#! @BeginExampleSession
+#! gap> Epsilonk(5);
+#! AbstractRegularPolytope([ 5, 2 ])
+#! gap> Epsilonk(6);
+#! AbstractRegularPolytope([ 6, 2 ])
+#! @EndExampleSession
 
 #! @Arguments k
 #! @Returns maniplex
 #! @Description Given an integer <A>k</A>, gives the map $\delta_k$, which is $\{2k,2\}/2$ when <A>k</A> is even, and $\{2k,2\}_{k}$ when <A>k</A> is odd.
 DeclareOperation("Deltak",[IsInt]);
+#! @BeginExampleSession
+#! gap> Deltak(5);
+#! AbstractRegularPolytope([ 10, 2 ], "(r0 r1)^5 r2")
+#! gap> Deltak(6);
+#! AbstractRegularPolytope([ 12, 2 ], "(r0 r1)^6 r2")
+#! @EndExampleSession
+
 
 #! @Arguments k
 #! @Returns maniplex
 #! @Description Given an integer <A>k</A>, gives the map $M_k$, which is $\{2k,2k\}_{1,0}$ when <A>k</A> is even, and $\{2k,k\}_{2}$ when <A>k</A> is odd.
 DeclareOperation("Mk",[IsInt]);
+#! @BeginExampleSession
+#! gap> Mk(5);Mk(6);
+#! AbstractRegularPolytope([ 10, 5 ], "(r0 r1)^5 r0 = r2")
+#! AbstractRegularPolytope([ 12, 12 ], "(r0 r1)^6 r0 = r2")
+#! @EndExampleSession
 
 #! @Arguments k
 #! @Returns maniplex
 #! @Description Given an integer <A>k</A>, gives the map $M'_k$, which is $\{k,k\}_2$ when <A>k</A> is even, and $\{k,2k\}_{2}$ when <A>k</A> is odd. 
 #! `MkPrime(k,i)` gives the map $M'_{k,i}$.
 DeclareOperation("MkPrime",[IsInt]);
+#! @BeginExampleSession
+#! gap> MkPrime(5);MkPrime(6);
+#! ReflexibleManiplex([ 5, 10 ], "(r2*r1*(r0 r2))^5,z1^2")
+#! ReflexibleManiplex([ 6, 6 ], "(r2*r1*(r0 r2))^6,z1^2")
+#! @EndExampleSession
 
 #! @Arguments k, l
 #! @Returns maniplex
 #! @Description Given integers <A>k,l</A>, gives the map $B(k,2l)$. 
 DeclareOperation("Bk2l",[IsInt,IsInt]);
+#! @BeginExampleSession
+#! gap> Bk2l(4,5);
+#! 3-maniplex with 80 flags
+#! @EndExampleSession
 
 #! @Arguments k, l
 #! @Returns maniplex
 #! @Description Given integers <A>k,l</A>, gives the map $B^*(k,2l)$. 
 DeclareOperation("Bk2lStar",[IsInt,IsInt]);
+#! @BeginExampleSession
+#! gap> Bk2lStar(5,7);
+#! 3-maniplex with 140 flags
+#! @EndExampleSession
 
 #! @Section Operators on reflexible maps
 
@@ -40,11 +71,19 @@ DeclareOperation("Bk2lStar",[IsInt,IsInt]);
 #! @Returns oppositeMap
 #! @Description Forms the opposite map of the maniplex <A>map</A>.
 DeclareOperation("Opp", [IsManiplex]);
+#! @BeginExampleSession
+#! gap> Opp(Bk2lStar(5,7));
+#! Petrial(Dual(Petrial(3-maniplex with 140 flags)))
+#! @EndExampleSession
 
 #! @Arguments map, j
 #! @Returns newMap
 #! @Description Given <A>map</A> and integer $j$, will form the map $H_j(map)$. Note that if the action of $[r_0,(r_1 r_2)^{j-1} r_1, r_2]$ on the flags forms multiple orbits, then the resulting map will be on just one of those orbits.
 DeclareOperation("Hole", [IsManiplex,IsInt]);
+#! @BeginExampleSession
+#! gap> Hole(Bk2lStar(5,7),2);
+#! 3-maniplex with 140 flags
+#! @EndExampleSession
 
 
 #! @Section Operations on Reflexible and Regular Maps
---
+++ tst/bicontactual.tst
@@ -0,0 +1,21 @@
+gap> Epsilonk(5);
+AbstractRegularPolytope([ 5, 2 ])
+gap> Epsilonk(6);
+AbstractRegularPolytope([ 6, 2 ])
+gap> Deltak(5);
+AbstractRegularPolytope([ 10, 2 ], "(r0 r1)^5 r2")
+gap> Deltak(6);
+AbstractRegularPolytope([ 12, 2 ], "(r0 r1)^6 r2")#! gap> Mk(5);Mk(6);
+AbstractRegularPolytope([ 10, 5 ], "(r0 r1)^5 r0 = r2")
+AbstractRegularPolytope([ 12, 12 ], "(r0 r1)^6 r0 = r2")
+gap> MkPrime(5);MkPrime(6);
+ReflexibleManiplex([ 5, 10 ], "(r2*r1*(r0 r2))^5,z1^2")
+ReflexibleManiplex([ 6, 6 ], "(r2*r1*(r0 r2))^6,z1^2")
+gap> Bk2l(4,5);
+3-maniplex with 80 flags
+gap> Bk2lStar(5,7);
+3-maniplex with 140 flags
+gap> Opp(Bk2lStar(5,7));
+Petrial(Dual(Petrial(3-maniplex with 140 flags)))
+gap> Hole(Bk2lStar(5,7),2);
+3-maniplex with 140 flags
