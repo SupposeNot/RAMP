d8639fa7aafb17c1435f71cb6bae9931caf00527
Gabe Cunningham <gabriel.cunningham@gmail.com>
2022-05-03 11:01:25 -0400

    Tweak the documenatation on mixing

--- lib/mixoperations.gd
+++ lib/mixoperations.gd
@@ -4,12 +4,15 @@
 #! @Section Mixing of Maniplexes functions
 
 #! @BeginGroup Mix
-#! @GroupTitle Mix of maniplexes
+#! @GroupTitle Mix of groups
+
 #! @Arguments permgroup, permgroup
 #! @Returns `IsGroup `. 
-#! @Description Given two (permutation) groups returns the mix of those groups.  Note, also works with FPgroups.
+#! @Description Given two groups (either both permutation groups or both FpGroups), returns the mix of those groups.  
 DeclareOperation("Mix", [IsPermGroup, IsPermGroup]);
 
+#! @Arguments fpgroup, fpgroup
+DeclareOperation("Mix", [IsFpGroup, IsFpGroup]);
 #! Here we build the mix of the connection groups of a 3-cube and an edge.
 #! @BeginExampleSession
 #! gap> g1:=ConnectionGroup(Cube(3));
@@ -19,30 +22,25 @@ DeclareOperation("Mix", [IsPermGroup, IsPermGroup]);
 #! gap> Mix(g1,g2);
 #! <permutation group with 3 generators>
 #! @EndExampleSession
+#! @EndGroup
 
-#! @Arguments fpgroup, fpgroup
-#! @Returns the Mix of two Finitely Presented groups gp and gq.
-DeclareOperation("Mix", [IsFpGroup, IsFpGroup]);
+#! @Arguments maniplex,  maniplex
+#! @Returns `IsReflexibleManiplex `. 
+#! @Description Given two maniplexes, returns their mix. For two reflexible maniplexes returns the IsReflexibleManiplex from the mix of their connection groups.  In general, it returns the "flag mix" of the two maniplexes (see `FlagMix`).
+DeclareOperation("Mix", [IsManiplex, IsManiplex]);
 
 
 #! @Arguments maniplex, maniplex
 #! @Returns `IsManiplex `. 
 #! @Description Given two maniplexes p, q this returns the maniplex of their  
-#! "flag" mix 
+#! "flag" mix. That is, it constructs the mix of their connection groups, keeps
+#! the connected component with the base flags of p and q, and then builds a maniplex
+#! from this.
 DeclareOperation("FlagMix", [IsManiplex, IsManiplex]);
 
 
 
-
-#! @Arguments maniplex,  maniplex
-#! @Returns `IsReflexibleManiplex `. 
-#! @Description Given two manpilexes, returns their mix. For two reflexible maniplexes returns the IsReflexibleManiplex from the mix of their connection groups.  In general, it returns the flag mix.
-
-DeclareOperation("Mix", [IsManiplex, IsManiplex]);
-
-#! @EndGroup
-
-#! @BeginGroup 
+#! @BeginGroup Comix
 #! @GroupTitle Comix
 
 #! @Arguments fpgroup, fpgroup
@@ -57,7 +55,7 @@ DeclareOperation("Comix", [IsReflexibleManiplex, IsReflexibleManiplex]);
 
 #! @EndGroup
 
-#! @BeginGroup
+#! @BeginGroup Tools
 #! @GroupTitle Indexed array tools
 #! @Arguments int, int, int, int
 #! @Returns `IsInteger `. 
