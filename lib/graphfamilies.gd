c473d5da92179e3be6cad969b798d657802c7d8e
Mark Mixer <84349042+Mark-Mixer-WIT@users.noreply.github.com>
2026-06-07 21:06:36 -0400

    Add example sessions

--- lib/graphfamilies.gd
+++ lib/graphfamilies.gd
@@ -4,24 +4,55 @@
 #! @Section Graph families
 
 #! @Returns `IsGraph`
-#! @Description Heawood Graph as described at https://www.distanceregular.org/graphs/heawood.html
+#! @Description Returns the Heawood graph, as described at https://www.distanceregular.org/graphs/heawood.html
 DeclareOperation("HeawoodGraph", []);
+#! @BeginExampleSession
+#! gap> h := HeawoodGraph();;
+#! gap> Length(Vertices(h));
+#! 14
+#! gap> Length(UndirectedEdges(h));
+#! 21
+#! @EndExampleSession
 
 #! @Returns `IsGraph`
-#! @Description Petersen Graph as described at https://www.gap-system.org/Manuals/pkg/grape/htm/CHAP002.htm
+#! @Description Returns the Petersen graph, as described at https://www.gap-system.org/Manuals/pkg/grape/htm/CHAP002.htm
 DeclareOperation("PetersenGraph", []);
+#! @BeginExampleSession
+#! gap> p := PetersenGraph();;
+#! gap> Length(Vertices(p));
+#! 10
+#! gap> Length(UndirectedEdges(p));
+#! 15
+#! @EndExampleSession
 
 #! @Arguments n, L
 #! @Returns `IsGraph`
-#! @Description Given an integer <A>n</A> and a list <A>L</A>, this returns the Circulant Graph with <A>n</A> vertices.
-#! For each `i` in the list <A>L</A> and each vertex `v`, there is an edge from `v` to `v+i` and `v-i` (mod <A>n</A>)
+#! @Description Given an integer <A>n</A> and a list <A>L</A>, this returns the circulant graph with vertices `[1..n]`.
+#! For each `i` in the list <A>L</A> and each vertex `v`, there is an edge from `v` to `v+i` and `v-i` modulo <A>n</A>.
 DeclareOperation("CirculantGraph", [IsInt,IsList]);
+#! @BeginExampleSession
+#! gap> c := CirculantGraph(6, [1,2]);;
+#! gap> Length(Vertices(c));
+#! 6
+#! gap> Length(UndirectedEdges(c));
+#! 12
+#! gap> IsConnectedGraph(c);
+#! true
+#! @EndExampleSession
 
 #! @Arguments n, m
 #! @Returns `IsGraph`
-#! @Description Given two integers <A>n, m</A>, this returns the Complete Bipartite Graph `K_{n,m}`.
+#! @Description Given two integers <A>n</A> and <A>m</A>, this returns the complete bipartite graph `K_{n,m}` with `n+m` vertices.
 DeclareOperation("CompleteBipartiteGraph", [IsInt,IsInt]);
-
+#! @BeginExampleSession
+#! gap> k := CompleteBipartiteGraph(3,4);;
+#! gap> Length(Vertices(k));
+#! 7
+#! gap> Length(UndirectedEdges(k));
+#! 12
+#! gap> IsBipartite(k);
+#! true
+#! @EndExampleSession
 
 
 
